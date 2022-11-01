dc10b53d025374c728409af20ec3d83617507b56
heechae96 <shinheechae@naver.com>
2022-11-01 16:01:53 +0900

    RequestParam 애너테이션

---
+++ src/main/java/com/fastcampus/ch2_intelliJ/MyDate.java
@@ -0,0 +1,42 @@
+package com.fastcampus.ch2_intelliJ;
+
+public class MyDate {
+    private int year;
+    private int month;
+    private int day;
+
+    // getter & setter
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        return "MyDate{" +
+                "year=" + year +
+                ", month=" + month +
+                ", day=" + day +
+                '}';
+    }
+}
--- target/ch2_intelliJ-1.0.0-BUILD-SNAPSHOT/WEB-INF/views/yoil.jsp
+++ target/ch2_intelliJ-1.0.0-BUILD-SNAPSHOT/WEB-INF/views/yoil.jsp
@@ -4,6 +4,9 @@
     <title>Title</title>
 </head>
 <body>
-    <p>${year}년 ${month}월 ${day}일은 ${yoil}요일입니다.</p>
+<%--    <h1>year = <%=request.getParameter("year")%></h1>--%>
+<%--    <p>${year}년 ${month}월 ${day}일은 ${yoil}요일입니다.</p>--%>
+<%--    getter와 setter를 만들어둬서 가능한것--%>
+    <h1>${myDate.year}년 ${myDate.month}월 ${myDate.day}일은 ${yoil}요일입니다.</h1>
 </body>
 </html>
