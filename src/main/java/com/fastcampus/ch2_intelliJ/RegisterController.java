a3a7dabade794c348ac907fe3a33bf37446396d6
heechae96 <shinheechae@naver.com>
2022-11-02 15:48:02 +0900

    redirect와 forward

--- src/main/java/com/fastcampus/ch2_intelliJ/RegisterController.java
+++ src/main/java/com/fastcampus/ch2_intelliJ/RegisterController.java
@@ -13,24 +13,25 @@ import java.net.URLEncoder;
 @Controller
 public class RegisterController {
     // 신규 회원가입 화면
-//    @RequestMapping(value = "/register/add", method = {RequestMethod.GET, RequestMethod.POST})
+    @RequestMapping(value = "/register/add", method = {RequestMethod.GET, RequestMethod.POST})
 //    @RequestMapping("/register/add")
     // 단순하게 화면만 보여준다. 하는 역할이 없음. 뷰컨트롤러로 바꿈.
 //    @GetMapping("/register/add")
-//    public String register() {
-//        return "registerForm";  // WEB-INF/views/registerForm.jsp
-//    }
+    public String register() {
+        return "registerForm";  // WEB-INF/views/registerForm.jsp
+    }
 
     // 회원 정보 등록
 //    @RequestMapping(value = "/register/save", method = RequestMethod.POST)
     @PostMapping("/register/save")  // 4.3부터
     public String save(User user, Model model) throws Exception {   // User앞에 @ModelAttribute생략
         // 1. 유효성 검사
-        if(!isValid(user)){
+        if (!isValid(user)) {
             String msg = URLEncoder.encode("잘못된 id입니다.", "utf-8");
 
             model.addAttribute("msg", msg);
-            return "redirect:/register/add";
+            return "forward:/register/add";
+//            return "redirect:/register/add";
 //            return "redirect:/register/add?msg="+msg;   // URL재작성(rewriting)
         }
 
@@ -39,6 +40,7 @@ public class RegisterController {
     }
 
     private boolean isValid(User user) {
-        return true;
+        return false;
+//        return true;
     }
 }
